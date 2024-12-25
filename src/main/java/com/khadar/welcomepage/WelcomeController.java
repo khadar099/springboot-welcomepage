02cbf5654f370678fc42a5ba670fe416e7ceee08
Khadar Basha <89116722+khadar099@users.noreply.github.com>
2024-12-25 23:49:16 +0530

    Update WelcomeController.java

--- src/main/java/com/khadar/welcomepage/WelcomeController.java
+++ src/main/java/com/khadar/welcomepage/WelcomeController.java
@@ -5,7 +5,6 @@ import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
 
-@SpringBootApplication
 @Controller
 public class WelcomeController {
 
