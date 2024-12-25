719267260614d9f93655c372e1beb3bd8d75f043
Khadar Basha <89116722+khadar099@users.noreply.github.com>
2024-12-25 22:34:18 +0530

    Add files via upload

---
+++ src/test/java/com/khadar/welcomepage/WelcomePageApplicationTests.java
@@ -0,0 +1,13 @@
+package com.khadar.welcomepage;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class WelcomePageApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
