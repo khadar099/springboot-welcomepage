0c06af92d92754dab37344618ad4d6700b7ca529
Khadar Basha <89116722+khadar099@users.noreply.github.com>
2026-03-19 17:53:44 +0530

    Create User.java

---
+++ src/main/java/com/khadar/welcomepage/model/User.java
@@ -0,0 +1,26 @@
+package com.khadar.welcomepage.model;
+
+import jakarta.persistence.*;
+import lombok.*;
+
+@Entity
+@Table(name = "users")
+@Getter
+@Setter
+@NoArgsConstructor
+@AllArgsConstructor
+@Builder
+public class User {
+
+    @Id
+    @GeneratedValue(strategy = GenerationType.IDENTITY)
+    private Long id;
+
+    @Column(unique = true)
+    private String email;
+
+    @Column(unique = true)
+    private String mobileNumber;
+
+    private String password;
+}
