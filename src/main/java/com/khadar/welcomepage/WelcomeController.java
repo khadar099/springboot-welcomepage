a796158ca412140e298568e0ded0942af96b124e
Khadar Basha <89116722+khadar099@users.noreply.github.com>
2026-03-19 17:51:42 +0530

    Update WelcomeController.java

--- src/main/java/com/khadar/welcomepage/WelcomeController.java
+++ src/main/java/com/khadar/welcomepage/WelcomeController.java
@@ -2,14 +2,12 @@ package com.khadar.welcomepage;
 
 import org.springframework.stereotype.Controller;
 import org.springframework.web.bind.annotation.GetMapping;
-import org.springframework.boot.SpringApplication;
-import org.springframework.boot.autoconfigure.SpringBootApplication;
 
 @Controller
 public class WelcomeController {
 
     @GetMapping("/")
     public String welcome() {
-        return "welcome"; // Refers to welcome.html (if using Thymeleaf)
+        return "welcome"; // Thymeleaf template: welcome.html
     }
 }
